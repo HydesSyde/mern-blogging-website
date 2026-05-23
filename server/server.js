3e59457d26d243cb0781528f1ca403a88bf356d7
Igbanesi michael <eshimi090@gmail.com>
2026-05-23 01:59:19 +0100

    adding cors to server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import cors from "cors";
 import http from "http";
 import health from "./health.js";
 import authRoutes from "./routes/auth.route.js";
@@ -9,6 +10,12 @@ import authRoutes from "./routes/auth.route.js";
 const app = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 app.use("/health", health);
 app.use("/api/auth", authRoutes);
 
